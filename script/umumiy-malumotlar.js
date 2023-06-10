cf34828d8c9faa619a41a6671de0251ad519f66e
Alisher <toshmurodov2001@mail.ru>
2023-06-10 11:28:40 +0500

    update

--- script/umumiy-malumotlar.js
+++ script/umumiy-malumotlar.js
@@ -25,10 +25,59 @@ const baholashObyektiningEgasi = document.querySelector(
   "#baholash-obyektining-egasi"
 );
 const baholashdanMaqsad = document.querySelector("#Baholashdan-maqsad");
+const baholovchiTashkilotNomi = document.querySelector('#Baholovchi-tashkilot-nomi');
+const baholovchiTashkilotIsmiSharifi = document.querySelector('#Baholovchi-tashkilot-ismi-sharifi');
+
 
 document
   .querySelector(".app-container__calculate-btn")
   .addEventListener("click", async function (e) {
+
+    const hisobotSanasi = new Date(hisobotTuzilganSanasi.value);
+    const hisobotSanasiOyi = (monthNum) => {
+      switch (Number(monthNum)) {
+        case 0:
+          return 'Yanvar';
+          break;
+        case 1:
+          return 'Fevral';
+          break;
+        case 2:
+          return 'Mart';
+          break;
+        case 3:
+          return 'Aprel';
+          break;
+        case 4:
+          return 'May';
+          break;
+        case 5:
+          return 'Iyun';
+          break;
+        case 6:
+          return 'Iyul';
+          break;
+        case 7:
+          return 'Avgust';
+          break;
+        case 8:
+          return 'Sentyabr';
+          break;
+        case 9:
+          return 'Oktyabr';
+          break;
+        case 10:
+          return 'Noyabr';
+          break;
+        case 11:
+          return 'Dekabr';
+          break;
+        default:
+          break;
+      }
+    }
+
+
     let myWindow = window.open("", "PRINT", "");
 
     myWindow.document.write(
@@ -36,6 +85,9 @@ document
         buyurtmachi: buyurtmachi.value,
         hisobotRoyhatiRaqami: hisobotRoyhatiRaqami.value,
         hisobotTuzilganSanasi: hisobotTuzilganSanasi.value,
+        hisobotSanasiKuni: hisobotSanasi.getDate(),
+        hisobotSanasiOyi: hisobotSanasiOyi(hisobotSanasi.getMonth()),
+        hisobotSanasiYili: hisobotSanasi.getFullYear(),
         baholashSanasi: baholashSanasi.value,
         avtotransportVositasiDavlatRaqami:
           avtotransportVositasiDavlatRaqami.value,
@@ -45,6 +97,8 @@ document
           baholashObyektiJoylashganManzili.value,
         baholashObyektiningEgasi: baholashObyektiningEgasi.value,
         baholashdanMaqsad: baholashdanMaqsad.value,
+        baholovchiTashkilotNomi: baholovchiTashkilotNomi.value,
+        baholovchiTashkilotIsmiSharifi: baholovchiTashkilotIsmiSharifi.value,
       })
     );
     myWindow.print();
