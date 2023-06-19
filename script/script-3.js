7a0f9d66a804dd75a8cd475bdbc38f1330f67fcf
Azizbek <azizraxmatov249@gmail.com>
2023-06-19 12:20:54 +0500

    updd

--- script/script-3.js
+++ script/script-3.js
@@ -764,6 +764,36 @@ eskirishEls.forEach(element => {
     })
 });
 // jamiEskirish.append(jamiEskirishlabel);
+const addInfoBtn1 = document.createElement("button");
+addInfoBtn1.setAttribute("class", "add_btn");
+addInfoBtn1.setAttribute("id", "add_btn");
+addInfoBtn1.textContent = `Ma'lumot qo'shish`;
+
+addInfoBtn1.addEventListener('click', function (e) {
+    const addMalumotBlock = document.createElement('div');
+    addMalumotBlock.setAttribute('class', 'app-container__input-option')
+
+
+    const addMalumotNomiLabel = document.createElement('label');
+    addMalumotNomiLabel.textContent = `Qoshimcha ma'lumot nomi:`;
+
+    const addMalumotNomi = document.createElement('input');
+    addMalumotNomi.setAttribute('type', 'text');
+
+
+    const addMalumotQiymatiabel = document.createElement('label');
+    addMalumotQiymatiabel.textContent = `Qoshimcha ma'lumot qiymati:`;
+
+    const addMalumotQiymati = document.createElement('input');
+    addMalumotQiymati.dataset.qiymatNomi = '';
+    addMalumotQiymati.setAttribute('id', 'add-malumot-qiymati');
+    addMalumotQiymati.setAttribute('name', 'add-malumot-qiymati');
+    addMalumotQiymati.setAttribute('type', 'number');
+
+    addMalumotBlock.append(addMalumotNomiLabel, addMalumotNomi, addMalumotQiymatiabel, addMalumotQiymati);
+    expertTahMethod.append(addMalumotBlock)
+    addInfoBtn1.before(addMalumotBlock);
+});
 
 /////////////////////////////////////////////////
 const expertTahMethodLabel5 = document.createElement('h3');
@@ -813,7 +843,7 @@ xarQiymatInput.setAttribute('type', 'number');
 
 xarQiymat.append(xarQiymatLabel, xarQiymatInput);
 /////////////////////////////////////////////////
-expertTahMethod.append(expertTahMethodLabel5, tiklanishQiymat, avtJamEskirish, xarQiymat)
+expertTahMethod.append(addInfoBtn1,expertTahMethodLabel5, tiklanishQiymat, avtJamEskirish, xarQiymat, )
 
 //expertTahMethodLabel4, jamiEskirish, expertTahMethodLabel3, jisEsk, tashqiEskirish, funcEskSab, tashqiEskSab,  funkEskirishMain,expertTahMethodLabel
 
